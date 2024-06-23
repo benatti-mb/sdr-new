93232b484e4e3c0fcc9e1a8752cba9081dd0beed
benatti-mb <benatti@segredosdarecorrencia.com.br>
2024-06-23 20:49:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 $(document).ready(function() {
     // URL para buscar os dados da planilha em formato JSON
-    var url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT8LgysYF-xRZwk2Udo38o72T4RAgSKWzpD2p1t0q-3mufv44uLDFTUZ9jVZNiCktmE89TtwP2UoaB9/pubhtml?gid=307821144&single=true';
+    var url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT8LgysYF-xRZwk2Udo38o72T4RAgSKWzpD2p1t0q-3mufv44uLDFTUZ9jVZNiCktmE89TtwP2UoaB9/pubhtml';
 
     $.getJSON(url, function(data) {
-        var headers = [];
-        var rows = [];
-
-        // Obtendo os cabeçalhos da planilha
-        data.query.tables.table.rows[0].c.forEach(function(cell) {
-            headers.push(cell.v);
-        });
-
-        // Obtendo os dados da planilha
-        for (var i = 1; i < data.query.tables.table.rows.length; i++) {
-            var row = data.query.tables.table.rows[i];
-            var rowData = [];
-            row.c.forEach(function(cell) {
-                rowData.push(cell.v);
-            });
-            rows.push(rowData);
+        // Tratando os dados recebidos
+        var rows = data.table.rows;
+        var headers = rows[0].c.map(function(cell) {
+            return cell.v;
         });
 
         // Preenchendo os cabeçalhos da tabela
@@ -30,14 +18,12 @@ $(document).ready(function() {
         $('#table-head').append(headerRow);
 
         // Preenchendo os dados da tabela
-        rows.forEach(function(rowData) {
-            var dataRow = '<tr>';
-            rowData.forEach(function(cellData) {
-                dataRow += '<td>' + cellData + '</td>';
+        for (var i = 1; i < rows.length; i++) {
+            var rowData = rows[i].c.map(function(cell) {
+                return '<td>' + cell.v + '</td>';
             });
-            dataRow += '</tr>';
-            $('#table-body').append(dataRow);
-        });
+            $('#table-body').append('<tr>' + rowData.join('') + '</tr>');
+        }
 
         // Inicialize o DataTable
         $('#spreadsheet').DataTable();
